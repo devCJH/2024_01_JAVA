fd2af711aa02d222152440467c9e4491152c0f42
devCJH <http-@naver.com>
2024-01-24 13:00:37 +0900

    article write

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
 
 class Main {
-	
-	static int in;
-	
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
 		
 		Scanner sc = new Scanner(System.in);
 		
+		int lastArticleId = 0;
+		
 		while(true) {
 			System.out.printf("명령어) ");
 			String cmd = sc.nextLine();
 			
-			System.out.println(cmd);
+			if (cmd.equals("article write")) {
+				lastArticleId++;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				System.out.printf("%d번 게시물이 생성되었습니다\n", lastArticleId);
+			}
 			
 			if (cmd.equals("exit")) {
 				break;
